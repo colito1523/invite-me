7cc38e9418f34db079cb7d9d56d2b33cb20fddb8
colomagan <colo.imperio@gmail.com>
2024-12-10 16:05:48 +0000

    el boton de voy o no voy ya anda

--- screens/BoxDetails/index.js
+++ screens/BoxDetails/index.js
@@ -78,6 +78,12 @@ export default memo(function BoxDetails({ route, navigation }) {
     };
   }, []);
 
+  useEffect(() => {
+    if (box) {
+      checkEventStatus({ box, selectedDate, setIsEventSaved });
+    }
+  }, [box, selectedDate]);
+
   const closeModal = () => {
     setModalVisible(false);
   };
