8986a1560a19f6826dc294e93de2fe781b6d5c11
colomagan <colo.imperio@gmail.com>
2025-01-20 00:27:31 +0000

    intentar subir

--- screens/ProfileComponents/MenuSection.js
+++ screens/ProfileComponents/MenuSection.js
@@ -24,6 +24,7 @@ const MenuSection = ({
       t("profileMenuSections.deleteAccountQuestions"),
       [
         {
+          
           text: t("chatUsers.cancel"),
           style: "cancel",
         },
