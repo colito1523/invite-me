a6270660d85753342801d4cea874d475c848c557
colomagan <colo.imperio@gmail.com>
2025-02-07 14:28:17 +0000

    actualizado el calendario

--- screens/CalendarPicker.js
+++ screens/CalendarPicker.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useDate } from "../src/hooks/DateContext"; // Importa el contexto
 import { View, Text, TouchableOpacity, Modal, StyleSheet, Dimensions } from "react-native";
 import { Calendar } from "react-native-calendars";
+import { useTranslation } from 'react-i18next';
+import { LocaleConfig } from 'react-native-calendars';
 import dayjs from "dayjs";
 import { LinearGradient } from "expo-linear-gradient";
 
@@ -11,6 +13,35 @@ const CalendarPicker = ({ onDateChange, setLoading }) => {
   const { selectedDate, setSelectedDate } = useDate(); // Usa el contexto
   const [modalVisible, setModalVisible] = useState(false);
   const [isNightMode, setIsNightMode] = useState(false);
+  const { i18n } = useTranslation();
+
+  useEffect(() => {
+    LocaleConfig.defaultLocale = i18n.language;
+  }, [i18n.language]); // Esto actualiza el idioma cuando cambia
+
+  LocaleConfig.locales['es'] = {
+    monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
+                 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+    monthNamesShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+    dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+    dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb'],
+  };
+
+  LocaleConfig.locales['pt'] = {
+    monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+                 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+    monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+    dayNames: ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'],
+    dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+  };
+
+  LocaleConfig.locales['en'] = LocaleConfig.locales[''] = {
+    monthNames: ['January', 'February', 'March', 'April', 'May', 'June',
+                 'July', 'August', 'September', 'October', 'November', 'December'],
+    monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+  };
 
   useEffect(() => {
     const checkTime = () => {
@@ -27,15 +58,16 @@ const CalendarPicker = ({ onDateChange, setLoading }) => {
   const maxDate = dayjs().add(2, "month").format("YYYY-MM-DD");
 
   const handleDayPress = (day) => {
-    const formattedDate = dayjs(day.dateString).format("D MMM");
+    const formattedDate = dayjs(day.dateString).format("D MMM"); // Formato "14 Feb"
     setSelectedDate(formattedDate);
+  
     setModalVisible(false);
-
     if (onDateChange) {
       setLoading(true);
-      onDateChange(formattedDate);
+      onDateChange(formattedDate); // Enviar en el nuevo formato a los otros componentes
     }
   };
+  
 
   const currentStyles = isNightMode ? nightStyles : dayStyles;
 
@@ -98,6 +130,7 @@ const CalendarPicker = ({ onDateChange, setLoading }) => {
     </View>
   );
 };
+
 
 const baseStyles = StyleSheet.create({
   dateButton: {
