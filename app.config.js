8bdb237576de450f408fac0a4cdc50167812c59b
colomagan <colo.imperio@gmail.com>
2025-03-14 07:27:04 +0000

    nueva 2

--- app.config.js
+++ app.config.js
@@ -16,7 +16,7 @@ export default {
   expo: {
     name: "Invite Me",
     slug: "invite-me",
-    version: "1.0.7",
+    version: "1.0.8",
     orientation: "portrait",
     icon: "./assets/nuevo_logo.png",
     userInterfaceStyle: "light",
